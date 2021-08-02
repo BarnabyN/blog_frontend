a66953f06743d1d499fbf5a8fc8e593f04645a35
Barnaby Napier <barnabynapier@gmail.com>
2021-08-02 21:00:38 +0100

    Update name

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -8,7 +8,7 @@ export default function CustomNavbar() {
     <div className="navbar">
       <nav expand="md" sticky="top">
         <Link className="navbrand" to={"/home"}>
-          G.P
+          B. Napier
         </Link>
         <span className="navtabs">
           <CustomNavlink to="/home" text="Home" />
